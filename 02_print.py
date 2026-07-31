539ae2843dbcf447aaf4c34052d2b2230c8547be
Gifter <midoku316@gmail.com>
2026-07-31 17:24:59 -0500

    Create 02_print.py

---
+++ 02_print.py
@@ -0,0 +1,14 @@
+# ==========================================
+# CONCEPTO: FUNCIÓN PRINT
+# ==========================================
+# Por convención, los nombres de funciones en Python se escriben en minúsculas.
+# Se usa la función print() para mostrar contenido o el valor de una variable en consola.
+
+# Las cadenas o strings son secuencias de caracteres rodeadas de comillas simples o dobles:
+msg = "Hello World!"
+print(msg) # Imprime: Hello World!
+
+# print() también puede recibir múltiples argumentos separados por coma:
+msg1 = "Hi"
+msg2 = "Hello"
+print(msg1, msg2) # Imprime: Hi Hello
