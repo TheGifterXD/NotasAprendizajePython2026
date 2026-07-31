195e54750af7ec40451dcece3f40044dbc8e69b4
Gifter <midoku316@gmail.com>
2026-07-31 18:00:20 -0500

    Update 02_print.py

--- 02_print.py
+++ 02_print.py
@@ -12,3 +12,6 @@ print(msg) # Imprime: Hello World!
 msg1 = "Hi"
 msg2 = "Hello"
 print(msg1, msg2) # Imprime: Hi Hello
+
+# Compilar: traduce el código a un lenguaje que pueda interpretar la máquina (como un libro de recetas chinas traducido a español)
+# Ejecutar: procesa las instrucciones compiladas (como leer el libro y preparar una receta)
